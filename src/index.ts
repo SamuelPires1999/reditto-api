1082550dd6da92e3560a5b84f053da219be85d33
Samuel Pires <samuel.ferpires@gmail.com>
2022-12-23 21:19:23 -0300

    feat: setup basic express server and utilities

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,22 @@
 import dotenv from "dotenv-safe";
-
+import Express, { Response, response } from "express";
+import cors from "cors";
+import morgan from "morgan";
+import helmet from "helmet";
 dotenv.config();
 
-console.log("Node js boilerplate is running");
+const main = async () => {
+  const app = Express();
+
+  app.use(cors());
+  app.use(helmet());
+  app.use(morgan("combined"));
+
+  app.listen(3000, () => {
+    console.log("Application running");
+  });
+};
+
+main().catch(err => {
+  console.log(err);
+});
