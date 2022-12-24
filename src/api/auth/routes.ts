7c9984fec261f5e6fdebf7de5914eee8f8a3ee89
Samuel Pires <samuel.ferpires@gmail.com>
2022-12-23 22:03:22 -0300

    feat: adding zod basic validation workflow

--- src/api/auth/routes.ts
+++ src/api/auth/routes.ts
@@ -1,11 +1,23 @@
 import { Router } from "express";
+import { signUpSchema } from "./schemas";
+import { ZodError } from "zod";
 
 const authRoutes = Router();
 
-authRoutes.get("/signUp", (req, res) => {
-  return res.json({
-    message: "SignUp route",
-  });
+authRoutes.post("/signUp", (req, res) => {
+  try {
+    const data = signUpSchema.parse(req.body);
+    return res.json({
+      data,
+    });
+  } catch (error) {
+    if (error instanceof ZodError) {
+      return res.json({
+        message: "Input Parse Error",
+        error,
+      });
+    } else return res.status(500).send("unknown server error");
+  }
 });
 
 authRoutes.get("/signIn", (req, res) => {
